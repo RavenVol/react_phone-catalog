6c16301a1fb592c7e5238fe95845e9b748b33346
RavenVol <r.volianiuk@gmail.com>
2019-08-08 15:06:10 +0300

    Updates

---
+++ precache-manifest.796099411136c5df47bf7224df54ef97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b61b6fc1b647cbcc651244e762da172",
+    "url": "/react_phone-catalog/index.html"
+  },
+  {
+    "revision": "83b3f04293509e4d122f",
+    "url": "/react_phone-catalog/static/css/main.97cfdc40.chunk.css"
+  },
+  {
+    "revision": "e80ec82bfa77a493ef8f",
+    "url": "/react_phone-catalog/static/js/2.d54d4320.chunk.js"
+  },
+  {
+    "revision": "83b3f04293509e4d122f",
+    "url": "/react_phone-catalog/static/js/main.73dddf78.chunk.js"
+  },
+  {
+    "revision": "9b3e1d68b9cc1dd9a925",
+    "url": "/react_phone-catalog/static/js/runtime~main.25bc40f7.js"
+  },
+  {
+    "revision": "4275dee916ee23eb29ce4356c2d2de24",
+    "url": "/react_phone-catalog/static/media/404.4275dee9.jpg"
+  }
+]);
